6654d6d7db6f7bc383452d4297701a5481fd7bbe
Gustavo Gomes <126426037+gomesgustavoo@users.noreply.github.com>
2024-05-13 10:46:35 -0300

    Update usuario_entidade.py

--- entidade/usuario_entidade.py
+++ entidade/usuario_entidade.py
@@ -1,6 +1,23 @@
 
-
 class UsuarioEntidade:
     def __init__(self, nome: str, usuario_id: int):
         self.__nome = nome
         self.__usuario_id = usuario_id
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome):
+        self.__nome == nome
+
+    @property
+    def usuario_id(self):
+        return self.__usuario_id
+    
+    @usuario_id.setter
+    def usuario_id(self, usuario_id):
+        self.__usuario_id = usuario_id
+        
+        
