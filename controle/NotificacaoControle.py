0f7a03890ebc4887c77077c23f5c947aefff698f
Gustavo Gomes <126426037+gomesgustavoo@users.noreply.github.com>
2024-05-13 11:18:20 -0300

    Create NotificacaoControle.py

---
+++ controle/NotificacaoControle.py
@@ -0,0 +1,24 @@
+from Entidade import NotificacaoEntidade
+
+
+class NotificacaoControle:
+    def __init__(self):
+        self.__notificacoes = []
+
+    def inclui_notificacao(self, tipo_notificacao, status):
+        nova_notificacao = NotificacaoEntidade(tipo_notificacao,status)
+        if nova_notificacao not in self.__notificacoes:
+            self.__notificacoes.append(nova_notificacao)
+    
+    def remove_notificacao(self, notificacao):
+        for notificacao in self.__notificacoes:
+            if isinstance(notificacao, NotificacaoEntidade):
+                self.__notificacoes.remove(notificacao)
+    
+    def altera_notificacao(self, notificacao, tipo_notificacao, status):
+        for notificacao in self.__notificacoes:
+            ...
+        
+    @property
+    def listar_notificacoes(self):
+        return self.__notificacoes
