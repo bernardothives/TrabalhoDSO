cc59cb2ca24a9c5c8300acfc383581d4d547e8e8
Gustavo Gomes <126426037+gomesgustavoo@users.noreply.github.com>
2024-05-13 10:57:20 -0300

    Create UsuarioControle.py

---
+++ controle/UsuarioControle.py
@@ -0,0 +1,26 @@
+from Entidade import UsuarioEntidade
+
+
+class UsuarioControle:
+    def __init__(self):
+        self.__usuarios = []
+
+    def inclui_usuario(self, nome, usuario_id):
+        novo_usuario = UsuarioEntidade(nome, usuario_id)
+        if novo_usuario not in self.__usuarios:
+            self.__usuarios.append(novo_usuario)
+
+    def remove_usuario(self, usuario: UsuarioEntidade):
+        if isinstance(usuario, UsuarioEntidade):
+            if usuario in self.__usuarios:
+                self.__usuarios.remove(usuario)
+
+    def procurar_usuario_id_por_nome(self, nome):
+        for usuario in self.__usuarios:
+            if usuario.nome == nome:
+                return usuario.usuario_id
+
+    @property
+    def listar_usuarios(self):
+        return self.__usuarios
+    
