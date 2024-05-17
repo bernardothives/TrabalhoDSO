bc7c0ac568cbb7804cd5e2e81d993177572cea9b
Bernardo Alves Thives <bernardo.thives@ufsc.br>
2024-05-17 20:29:16 -0300

    if isinstance

--- entidade/alerta_entidade.py
+++ entidade/alerta_entidade.py
@@ -3,21 +3,25 @@
 class Alerta:
 
     def __init__(self, tipo_alerta: str, severidade: str):
-        self.__tipo_alerta = tipo_alerta
-        self.__severidade = severidade
+        if isinstance(tipo_alerta, str):
+            self.__tipo_alerta = tipo_alerta
+        if isinstance(severidade, str):
+            self.__severidade = severidade
 
     @property
     def tipo_alerta(self):
         return self.__tipo_alerta
 
     @tipo_alerta.setter
-    def tipo_alerta(self, tipo_alerta):
-        self.__tipo_alerta = tipo_alerta
+    def tipo_alerta(self, tipo_alerta: str):
+        if isinstance(tipo_alerta, str):
+            self.__tipo_alerta = tipo_alerta
 
     @property
     def severidade(self):
         return self.__severidade
 
     @severidade.setter
-    def severidade(self, severidade):
-        self.__severidade = severidade
+    def severidade(self, severidade: str):
+        if isinstance(severidade, str):
+            self.__severidade = severidade
