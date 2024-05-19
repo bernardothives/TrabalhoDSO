9f3108b1c4a00dca4778c91194a6a915bea6991d
Bernardo Thives <bernardoalvesthives@gmail.com>
2024-05-18 22:52:56 -0300

    aiaiai

--- controle/clima_atual_controle.py
+++ controle/clima_atual_controle.py
@@ -1,8 +1,30 @@
 from entidade.clima_atual_entidade import ClimaAtualEntidade
 from limite.clima_atual_tela import ClimaAtualTela
+from controle.clima_controle_abstrato import ClimaControleAbstrato
 
 
-class ClimaAtualControle:
-    def __init__(self):
+class ClimaAtualControle(ClimaControleAbstrato):
+    def __init__(self, sistema):
+        super().__init__(sistema)
         self.__log_atual = []
         self.__clima_atual_tela = ClimaAtualTela()
+
+    def ver_dados_climaticos(self):
+        pass
+
+    def adiciona_log(self):
+        pass
+
+    def lista_log(self):
+        pass
+
+    def apaga_log(self):
+        pass
+
+    def apaga_log_especifico(self):
+        pass
+
+    def abre_tela(self):
+        lista_opcoes = {1: self.ver_dados_climaticos, 2: self.lista_log,
+                        3: self.apaga_log, 4: self.apaga_log_especifico,
+                        0: self.retornar}
--- limite/clima_atual_tela.py
+++ limite/clima_atual_tela.py
@@ -1,12 +1,21 @@
 class ClimaAtualTela:
-    def tela_opcoes(self):
+    @staticmethod
+    def tela_opcoes():
         print("-------- Clima Atual ----------")
-        print("str")
-        print("1 - str")
-        print("2 - str")
-        print("3 - str")
-        print("4 - str")
+        print("Escolha a opcao")
+        print("1 - Ver Dados Climáticos")
+        print("2 - Ver Histórico de Registro de Climas")
+        print("3 - Apagar Todo o Histórico de Registro")
+        print("4 - Apagar Registro de Clima Especifico")
         print("0 - Retornar")
 
         opcao = int(input("Escolha a opcao: "))
         return opcao
+
+    @staticmethod
+    def pega_dados_ver_clima():
+        print("-------- DADOS PARA VER O CLIMA ----------")
+        cpf = input("Seu CPF: ")
+        localizacao = input("Localização: ")
+
+        return {"cpf": cpf, "codigo": localizacao}
