cd52b4aa44142156c9cddc6540841e8a36f3bed3
bernardothives <bernardoalvesthives@gmail.com>
2024-06-23 16:42:26 -0300

    haha

--- limite/sistema_tela.py
+++ limite/sistema_tela.py
@@ -1,44 +1,53 @@
 from limite.tela_abstrata import TelaAbstrata
 import PySimpleGUI as sg
 
+
 class SistemaTela(TelaAbstrata):
     def __init__(self):
         self.__window = None
         self.init_components()
 
     def tela_opcoes(self):
-        self.init_components()
-        button, values = self.__window.Read()
-        opcao = 0
-        if values['1']:
-            opcao = 1
-        if values['2']:
-            opcao = 2
-        if values['3']:
-            opcao = 3
-        if values['4']:
-            opcao = 4
-        if values['5']:
-            opcao = 5
-        if values['0'] or button in (None, 'Cancelar'):
+        while True:
+            event, values = self.__window.read()
             opcao = 0
+            if event in (None, 'Cancelar'):
+                opcao = 0
+                break
+            if values['1']:
+                opcao = 1
+                break
+            if values['2']:
+                opcao = 2
+                break
+            if values['3']:
+                opcao = 3
+                break
+            if values['4']:
+                opcao = 4
+                break
+            if values['5']:
+                opcao = 5
+                break
+            if values['0']:
+                opcao = 0
+                break
         self.close()
         return opcao
 
     def init_components(self):
-        sg.ChangeLookAndFeel('LightBlue')
+        sg.theme('LightBlue2')  # Tema mais claro e suave
         layout = [
-            [sg.Text('------ CLIMA CO. ------', font=("Helvica", 25))],
-            [sg.Text('Escolha sua opção:', font=("Helvica", 15))],
-            [sg.Radio('Usuário', "RD1", key='1')],
-            [sg.Radio('Localização', "RD1", key='2')],
-            [sg.Radio('Clima Atual', "RD1", key='3')],
-            [sg.Radio('Previsão do Clima', "RD1", key='4')],
-            [sg.Radio('Notificação', "RD1", key='5')],
-            [sg.Radio('Finalizar o Sistema', "RD1", key='0')],
-            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+            [sg.Text('CLIMA CO', font=("Helvetica", 28), justification='center', pad=(10, 20), text_color='navy')],
+            [sg.Text('Escolha sua opção:', font=("Helvetica", 16), pad=(10, 10), text_color='darkblue')],
+            [sg.Radio('Usuário', "RD1", key='1', font=("Helvetica", 14), pad=(10, 5), enable_events=True, text_color='black')],
+            [sg.Radio('Localização', "RD1", key='2', font=("Helvetica", 14), pad=(10, 5), enable_events=True, text_color='black')],
+            [sg.Radio('Clima Atual', "RD1", key='3', font=("Helvetica", 14), pad=(10, 5), enable_events=True, text_color='black')],
+            [sg.Radio('Previsão do Clima', "RD1", key='4', font=("Helvetica", 14), pad=(10, 5), enable_events=True, text_color='black')],
+            [sg.Radio('Notificação', "RD1", key='5', font=("Helvetica", 14), pad=(10, 5), enable_events=True, text_color='black')],
+            [sg.Radio('Finalizar o Sistema', "RD1", key='0', font=("Helvetica", 14), pad=(10, 5), enable_events=True, text_color='black')]
         ]
-        self.__window = sg.Window('Clima CO.').Layout(layout)
+        self.__window = sg.Window('Clima CO', layout, element_justification='c', finalize=True)
 
     def close(self):
-        self.__window.Close()
+        self.__window.close()
